e817a7108dda1ac2c219f3c347372b754a0fec16
NaiHua Chen <vip899629@gmail.com>
2021-06-04 16:30:24 +0800

    修正

--- js/all.js
+++ js/all.js
@@ -126,73 +126,73 @@ function hotAreaCardItem(e){
 }
 
 //頁數
-function pagination(locationDataArry , nowPage){
-    //取得資料長度
-    const dataTotal = locationDataArry.length;
-    //顯示在畫面上的資料數量，預設每一頁只顯示五筆資料。
-    const perPage = 6;
+// function pagination(locationDataArry , nowPage){
+//     //取得資料長度
+//     const dataTotal = locationDataArry.length;
+//     //顯示在畫面上的資料數量，預設每一頁只顯示五筆資料。
+//     const perPage = 6;
 
-    //page 數量公式：總資料數 / 每一顯示資料 (perPage)
-    //可能會出現餘數，要使用 無條件進位
-    const pageTotal = Math.ceil(dataTotal / perPage); //總頁數
+//     //page 數量公式：總資料數 / 每一顯示資料 (perPage)
+//     //可能會出現餘數，要使用 無條件進位
+//     const pageTotal = Math.ceil(dataTotal / perPage); //總頁數
 
-    /*當前頁數*/
-    //要避免「當前頁數」比「總頁數」還要多，假設今天「總頁數」是 3 筆，就不可能是 4 或 5 的情況，所以要寫入一個判斷避免這種狀況
-    let currentPage = nowPage; //當前頁面
+//     /*當前頁數*/
+//     //要避免「當前頁數」比「總頁數」還要多，假設今天「總頁數」是 3 筆，就不可能是 4 或 5 的情況，所以要寫入一個判斷避免這種狀況
+//     let currentPage = nowPage; //當前頁面
 
-    //當「當前頁面」比「總頁數」大時，「當前頁面」就等於「總頁數」
-    if(currentPage > pageTotal){
-        currentPage = pageTotal;
-    }
+//     //當「當前頁面」比「總頁數」大時，「當前頁面」就等於「總頁數」
+//     if(currentPage > pageTotal){
+//         currentPage = pageTotal;
+//     }
 
-    /* 切換頁面時 */
-    //第一頁：1 ~ 6 ; 第二頁：7 ~ 12
+//     /* 切換頁面時 */
+//     //第一頁：1 ~ 6 ; 第二頁：7 ~ 12
 
-    //最小值公式：(當前頁面:2 * 預設資料數)- 預設資料數 + 1 此時會得到 6 這個數字，但是我們是第 7 筆開始，所以要在 +1
-    const minData = (currentPage * perPage) - perPage + 1;
-    //最大值公式：12
-    const maxData = (currentPage * perPage);
+//     //最小值公式：(當前頁面:2 * 預設資料數)- 預設資料數 + 1 此時會得到 6 這個數字，但是我們是第 7 筆開始，所以要在 +1
+//     const minData = (currentPage * perPage) - perPage + 1;
+//     //最大值公式：12
+//     const maxData = (currentPage * perPage);
 
-    //建立新陣列
-    let newData = [];
-    locationDataArry.forEach(function(item , index){
-        let num = index + 1; //因為索引值是從 0 開始所以要加 1
-        //當 num 大於等於 minData 且 小於等於 maxData ，就 push 新陣列
-        if(num >= minData && num <= maxData){
-            newData.push(item);
-        }
-    })
+//     //建立新陣列
+//     let newData = [];
+//     locationDataArry.forEach(function(item , index){
+//         let num = index + 1; //因為索引值是從 0 開始所以要加 1
+//         //當 num 大於等於 minData 且 小於等於 maxData ，就 push 新陣列
+//         if(num >= minData && num <= maxData){
+//             newData.push(item);
+//         }
+//     })
 
-    //因為要將分頁相關資訊傳到另一個 function 做處理，所以將 page 相關所需要的東西改用物件傳遞。
-    const page = {
-        pageTotal,
-        currentPage,
-        hasPage:currentPage > 1,
-        hasNext:currentPage < pageTotal,
-    }
-    pageBtn(page);
-}
+//     //因為要將分頁相關資訊傳到另一個 function 做處理，所以將 page 相關所需要的東西改用物件傳遞。
+//     const page = {
+//         pageTotal,
+//         currentPage,
+//         hasPage:currentPage > 1,
+//         hasNext:currentPage < pageTotal,
+//     }
+//     pageBtn(page);
+// }
 
-function pageBtn(page){
-    let str= '';
-    let total = page.pageTotal;
-    let currentPage = page.currentPage;
-    if(page.hasPage){
-        str += `<li class="prev"><a href="#" data-page="${Number(page.currentPage) - 1}">Prev</a></li>`;
-    }
-    for(let i=1; i<=total; i++){
-        if(i == currentPage){
-            str += `<li><a href="#" class="active" data-page="${i}">${i}</a></li>`;
-        }
-        else if(Math.ceil(i/6) == Math.ceil(currentPage/6)){
-            str += `<li><a href="#" data-page="${i}">${i}</a></li>`;
-        }
-    }
-    if(page.hasNext){
-        str += `<li class="next"><a href="#" data-page="${Number(page.currentPage) + 1}">Next</a></li>`;
-    }
-    pageNav.innerHTML = str;
-}
+// function pageBtn(page){
+//     let str= '';
+//     let total = page.pageTotal;
+//     let currentPage = page.currentPage;
+//     if(page.hasPage){
+//         str += `<li class="prev"><a href="#" data-page="${Number(page.currentPage) - 1}">Prev</a></li>`;
+//     }
+//     for(let i=1; i<=total; i++){
+//         if(i == currentPage){
+//             str += `<li><a href="#" class="active" data-page="${i}">${i}</a></li>`;
+//         }
+//         else if(Math.ceil(i/6) == Math.ceil(currentPage/6)){
+//             str += `<li><a href="#" data-page="${i}">${i}</a></li>`;
+//         }
+//     }
+//     if(page.hasNext){
+//         str += `<li class="next"><a href="#" data-page="${Number(page.currentPage) + 1}">Next</a></li>`;
+//     }
+//     pageNav.innerHTML = str;
+// }
 
 function switchPage(e){
     e.preventDefault();
@@ -200,7 +200,7 @@ function switchPage(e){
         return;
     }
     let page = e.target.dataset.page;
-    pagination(locationDataArry, page);
+    
 }
 
 goTop.addEventListener('click',function(e){
